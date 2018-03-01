dc2c53429b52746d551826d354a07c6efe0b3051
Luca Montagnoli <luca.montagnoli@rgigroup.com>
2018-03-01 23:22:31 +0100

    Lines ok

--- LateAtWork/src/main/java/com/late/work/Line.java
+++ LateAtWork/src/main/java/com/late/work/Line.java
@@ -1,5 +1,7 @@
 package com.late.work;
 
+import java.awt.geom.Line2D;
+
 public class Line {
 
 	private Point p1;
@@ -10,19 +12,49 @@ public class Line {
 		this.p2 = p2;
 	}
 
+	public Line(int x1, int y1, int x2, int y2) {
+		this(new Point(x1, y1),new Point(x2, y2));
+	}
+
 	public Point getP1() {
 		return p1;
 	}
 
 	public Point getP2() {
 		return p2;
-	}	
+	}
 
+	public boolean isVertical() {
+		return p1.getX() == p2.getX();
+	}
+
+	public boolean isHorizontal() {
+		return p1.getY() == p2.getY();
+	}
+	
 	public boolean intersect(Line line2) {
-		Point l2_p1 = line2.getP1();
-		Point l2_p2 = line2.getP2();
-		// to do
-		return false;
+		int x1a = p1.getX();
+		int y1a = p1.getY();
+		int x2a = p2.getX();
+		int y2a = p2.getY();
+		int x1b = line2.getP1().getX();
+		int y1b = line2.getP1().getY();
+		int x2b = line2.getP2().getX();
+		int y2b = line2.getP2().getY();
+		return Line2D.linesIntersect(x1a, y1a, x2a, y2a, x1b, y1b, x2b, y2b);
+	}
+	
+	/**
+	 * Check if num is between n1 and n2 (no matter which is the greater)
+	 * @param num
+	 * @param n1
+	 * @param n2
+	 * @return
+	 */
+	private boolean between(int num, int n1, int n2) {
+		int nA = (n1 < n2) ? n1 : n2;
+		int nB = (n1 < n2) ? n2 : n1;
+		return (num > nA && num < nB);
 	}
 
 }
